643c69ee25c12c1003aa204220403084bb1ab0e6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-23 14:14:37 +0300

    Added user functionality and update password

--- property_manager/core/user/serializers.py
+++ property_manager/core/user/serializers.py
@@ -1,4 +1,6 @@
 from utils.validators import validate_password_contain_digit, validate_password_contain_uppercase_letter, validate_password_contain_lowercase_letter, validate_password_contain_symbol
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.password_validation import validate_password as v_passwords
 
 from django.core import exceptions
 from core.user.models import AppUser
@@ -7,6 +9,11 @@ from django.contrib.auth import password_validation as validators
 
 
 class UserSerializer(serializers.ModelSerializer):
+    __doc__ = _(
+        """
+        Serializer for User model
+        """
+    )
 
     class Meta:
         model = AppUser
@@ -32,6 +39,46 @@ class UserSerializer(serializers.ModelSerializer):
         if errors:
            raise serializers.ValidationError(errors)
         return super().validate(data)
+
+
+class UserPwdChangeSerializer(serializers.Serializer):
+    __doc__ = _(
+        """
+         Serializer for user model password change
+        """
+    )
+
+    old_password = serializers.CharField(max_length=128, write_only=True, required=True)
+    new_password1 = serializers.CharField(max_length=128, write_only=True, required=True)
+    new_password2 = serializers.CharField(max_length=128, write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['user']
+        if not user.check_password(value):
+            raise serializers.ValidationError(
+                _('Your old password was entered incorrectly. Please enter it again.')
+            )
+        return value
+    
+    def validate(self, data):
+        if data['new_password1'] != data['new_password2']:
+            raise serializers.ValidationError({'new_password2': _("The two password fields didn't match.")})
+
+        validate_password_contain_digit(data['new_password1'])
+        validate_password_contain_uppercase_letter(data['new_password1']) 
+        validate_password_contain_lowercase_letter(data['new_password1'])
+        validate_password_contain_symbol(data['new_password1'])
+        v_passwords(data['new_password1'], self.context['user'])
+        return data    
+    
+    def save(self, **kwargs):
+        password = self.validated_data['new_password1']
+        user = self.context['user']
+        user.set_password(password)
+        user.save()
+        return user
+
+
 
 
 
