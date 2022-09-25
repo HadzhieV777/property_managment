fce86e7b48645ec3aff84f93f42f1234db72ce22
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-09-25 15:13:07 +0300

    Edited serializers.py

--- property_manager/core/user/serializers.py
+++ property_manager/core/user/serializers.py
@@ -82,10 +82,3 @@ class UserPwdChangeSerializer(serializers.Serializer):
 
 
 
-#    https://dev.to/koladev/django-rest-authentication-cmh
-#    https://jpmeyer.dev/blog/django-rest-framework-custom-users
-#    https://krakensystems.co/blog/2020/custom-users-using-django-rest-framework
-#    https://kimmosaaskilahti.fi/blog/2021-04-02-django-custom-user/
-#    https://www.remoteinning.com/blog/how-to-use-jwt-authentication-with-django-rest-framework
-#    https://hackernoon.com/110percent-complete-jwt-authentication-with-django-and-react-2020-iejq34ta
-#    https://rental.turbotenant.com/owners/dashboard
