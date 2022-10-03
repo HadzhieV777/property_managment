f245ea3608b7bd8992a8d4459f05c9e159542625
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-10-03 13:32:44 +0300

    Refractoring user model

--- property_manager/core/user/models.py
+++ property_manager/core/user/models.py
@@ -7,14 +7,15 @@ from core.user.manager import AppUserManager
 class AppUser(AbstractUser, PermissionsMixin):
     username = None
     email = models.EmailField(db_index=True, unique=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    
+    objects = AppUserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = AppUserManager()
 
-    phone_number = models.IntegerField(blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.email}"
