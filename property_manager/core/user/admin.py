bb36940e956a6c8fa6b1d3f72a2d62b381e92c48
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-10-13 15:30:43 +0300

    Added property model

--- property_manager/core/user/admin.py
+++ property_manager/core/user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AppUser
 
+
+@admin.register(AppUser)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number', 'first_name', 'last_name')
 
