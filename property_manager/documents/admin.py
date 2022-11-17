bc42f48e4affff792cb028ea835c7f4efeff4b83
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 21:37:49 +0200

    Generated new app

---
+++ property_manager/documents/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# from .models import SingleDocument
+
+
+# @admin.register(SingleDocument)
+# class PropertyAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'realtor', 
+#         'tenant',
+#         'first_photo',
+#         'second_photo',
+#         'third_photo',
+#         'forth_photo',
+#         'fifth_photo'
+#         )
