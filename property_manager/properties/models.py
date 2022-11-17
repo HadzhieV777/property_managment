efac9ef02bd94561e8736343e65cd20ae2a019e4
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-17 21:37:24 +0200

    Edited property model

--- property_manager/properties/models.py
+++ property_manager/properties/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+# from documents.models import SingleDocument
+
+
 
 UserModel = get_user_model()
 
@@ -32,7 +35,9 @@ class SingleProperty(models.Model):
     price = models.FloatField()
     baths = models.IntegerField()
     square_feet = models.FloatField()
-    year_built = models.IntegerField()
+    year_built = models.IntegerField(null=True, blank=True)
     pets = models.BooleanField()
+    need_maintenace = models.BooleanField(null=True, blank=True)
     realtor = models.ForeignKey(UserModel, related_name='realtor', on_delete=models.CASCADE)
     tenant = models.ForeignKey(UserModel, null=True, blank=True, related_name='tenant',  on_delete=models.SET_NULL)
+    # documents = models.ForeignKey(SingleDocument, related_name='document', on_delete=models.CASCADE, null=True, blank=True)
