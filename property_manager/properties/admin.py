3e25e29a11cc55e3fe5cb83d78f2245d7d292283
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-10 17:29:35 +0200

    Refactoring estate model

--- property_manager/properties/admin.py
+++ property_manager/properties/admin.py
@@ -4,5 +4,5 @@ from .models import SingleProperty
 
 @admin.register(SingleProperty)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('address', 'city', 'type', 'status', 'tenant', 'price')
+    list_display = ('address', 'baths', 'bedrooms', 'city', 'maintenance', 'owner', 'price', 'rented', 'type', 'zip')     
 
