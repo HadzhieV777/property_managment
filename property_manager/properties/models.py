7ca37e250517ba253933c3b9965306fcaa05ed04
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-10 17:30:15 +0200

    Refactoring estate model

--- property_manager/properties/models.py
+++ property_manager/properties/models.py
@@ -17,27 +17,39 @@ class SingleProperty(models.Model):
     address = models.CharField(
         max_length=ADDRESS_MAX_LEN
     )
-    unit = models.IntegerField()
+
+    baths = models.IntegerField()
+
+    bedrooms = models.IntegerField() 
 
     city = models.CharField(
         max_length=CITY_MAX_LEN
     )
-    zip_code = models.CharField(
-        max_length=ZIP_MAX_LEN
+    maintenance = models.BooleanField(
+        null=True, 
+        blank=True
     )
+
+    owner = models.ForeignKey(UserModel, related_name='realtor', on_delete=models.CASCADE)
+
+    price = models.IntegerField()
+
+    rented = models.BooleanField(
+        null=True, 
+        blank=True
+    )
+    
     type = models.CharField(
         max_length=TYPE_MAX_LEN
     )
-    status = models.CharField(
-        max_length=STATUS_MAX_LEN
+
+    zip = models.CharField(
+        max_length=ZIP_MAX_LEN
     )
-    bedrooms =models.IntegerField()
-    price = models.FloatField()
-    baths = models.IntegerField()
+
     square_feet = models.FloatField()
-    year_built = models.IntegerField(null=True, blank=True)
-    pets = models.BooleanField()
-    need_maintenace = models.BooleanField(null=True, blank=True)
-    realtor = models.ForeignKey(UserModel, related_name='realtor', on_delete=models.CASCADE)
-    tenant = models.ForeignKey(UserModel, null=True, blank=True, related_name='tenant',  on_delete=models.SET_NULL)
-    # documents = models.ForeignKey(SingleDocument, related_name='document', on_delete=models.CASCADE, null=True, blank=True)
+
+    pets = models.BooleanField(
+        null=True, 
+        blank=True
+    )
